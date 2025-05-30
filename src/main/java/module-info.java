54fe2c1a211a4a5142f1923cf0a64e7191034c8c
Afia Yeboah <aforiwaa2015@gmail.com>
2025-05-30 19:43:09 -0400

    Initial Commit with JavaFx Templates

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment1gc200624860 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment1gc200624860 to javafx.fxml;
+    exports com.example.assignment1gc200624860;
+}
